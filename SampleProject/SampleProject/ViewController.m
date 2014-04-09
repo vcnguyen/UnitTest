32312bbf368f50f8ecd88360f5d6ed8523450ae3
nguyen.vo <nguyen.vo@konektasoft.com>
2014-04-09 12:01:01 +0700

    New Project

---
+++ SampleProject/SampleProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SampleProject
+//
+//  Created by Chi nguyen Vo on 4/9/14.
+//  Copyright (c) 2014 Chi nguyen Vo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
