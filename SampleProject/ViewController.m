c4c5ecadc6298df83c8bb4b644fa52dcf962d64f
Chi nguyen Vo <chinguyenvo@Chis-MacBook-Air.local>
2014-04-10 01:13:29 +0700

    animation

--- SampleProject/ViewController.m
+++ SampleProject/ViewController.m
@@ -17,7 +17,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    UIImage *image = [UIImage imageNamed:@"HongNi"];
+    UIImageView *imageView = [[UIImageView alloc]initWithImage:image];
+    [self.view addSubview:imageView];
+    imageView.frame = CGRectMake(50, 50, 100, 100);
+    imageView.contentMode = UIViewContentModeScaleAspectFit;
+    imageView.alpha = 0;
+    
+    [UIView animateWithDuration:3 animations:^{
+        imageView.alpha = 1;
+    }completion:^(BOOL finished) {
+        [UIView animateWithDuration:2 animations:^{
+            imageView.transform = CGAffineTransformMakeRotation(360/180*M_PI_2);
+        }completion:^(BOOL finished) {
+            [UIView animateWithDuration:2 animations:^{
+                imageView.transform = CGAffineTransformMakeRotation(0/180*M_PI_2);
+            }];
+        }];
+    }];
 }
 
 - (void)didReceiveMemoryWarning
