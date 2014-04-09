0c2742c702d19fe2d8c230a438864155079285c5
Chi nguyen Vo <chinguyenvo@Chis-MacBook-Air.local>
2014-04-10 00:50:40 +0700

    New

---
+++ SampleProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SampleProject
+//
+//  Created by Chi nguyen Vo on 4/9/14.
+//  Copyright (c) 2014 Chi nguyen Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
